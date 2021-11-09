cc1856cbcb9f78a9f7f59e4b1b510f4e78ddc95e
merisaar <mtmsaa@utu.fi>
2021-11-09 21:07:55 +0200

    Add option to post new instances to the food ingredients API

--- ingredients_backend/food_information/models.py
+++ ingredients_backend/food_information/models.py
@@ -4,6 +4,7 @@ import json
 
 class FoodEntry(models.Model):
     ean = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=500)
     allergens = models.CharField(max_length=200)
 
