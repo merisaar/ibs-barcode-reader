190ff8824dc7d652acf1ccfbafcca9ea4e777409
Meri Saarinen <mtmsaa@utu.fi>
2021-11-10 22:57:06 +0200

    Remove ingredients_backend and move it its own repo

--- ingredients_backend/food_information/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FoodInformationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'food_information'
--- ingredients_backend/food_information/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from food_information.views import FoodEntryListView, FoodEntryDetailView, CleanDataBase
-
-urlpatterns = [
-    path('', FoodEntryListView.as_view()),
-    path('<str:ean_parameter>/', FoodEntryDetailView.as_view()),
-    path('cleanDB', CleanDataBase.as_view()),
-]
